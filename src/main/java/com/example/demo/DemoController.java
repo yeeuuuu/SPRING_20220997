a9912d025b8d59d51bc78b31cca16d819f5d57e8
admin <ye6585055@gmail.com>
2025-09-10 23:53:48 +0900

    2주차 연습문제 및 요약 정리 완료

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,33 @@
+//demo 패키지 폴더 안에 컨트롤러 파일을 만들어야 함. (패키지 : 프로젝트 폴더 구조 구분)
+//대 소문자 및 이름이 일치해야 한다.  
+//컨트롤러를 통해 페이지에 접근 == 페이지를 만들때마다 컨트롤러 등록 필요
+//페이지 이름(주소)로 접근 가능하면 보안이 취약함. URI 컨트롤러 사용해 접근하면 서버 보안 강화
+
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // 컨트롤러 어노테이션 명시
+public class DemoController
+{
+    @GetMapping("/hello") // 전송 방식 GET
+    public String hello(Model model) {
+        model.addAttribute("data", " 방갑습니다."); // model 설정
+        return "hello"; // hello.html 연결
+    }
+
+    @GetMapping("/hello2") // 전송 방식 GET
+    public String hello2(Model model) {
+        model.addAttribute("data1", " 홍길동님."); // model 설정
+        model.addAttribute("data2", " 반갑습니다."); // model 설정
+        model.addAttribute("data3", " 오늘."); // model 설정
+        model.addAttribute("data4", " 날씨는."); // model 설정
+        model.addAttribute("data5", " 매우 좋습니다."); // model 설정
+        return "hello2"; // 경로 요청이 오면, html 연결
+    }
+} 
+
+//컨트롤러에서 사용자에게 응답할 View를 생성할 때 Model을 이용하여 View에 데이터를 전달
+//model.addAttribute(String name/key, Object value);
